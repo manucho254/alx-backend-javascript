428ed81576a78e54cac1ca09af569b425e0acdc2
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-04 02:20:27 +0300

    Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,9 +2,9 @@
 
 export default function handleResponseFromAPI(promise) {
   promise
-    .then(() => {
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => {
       console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    })
-    .catch(Error());
+    });
 }
