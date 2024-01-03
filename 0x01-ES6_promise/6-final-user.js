7dab8605f3c71e21cc268c0baf2e849394026bcc
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-04 00:25:16 +0300

    code fixes

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,5 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup() {
-  return Promise.all([signUpUser(), uploadPhoto()]);
+  return Promise.allSettled([signUpUser(), uploadPhoto()]);
 }
---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+/* eslint-disable no-unused-vars */
+
+export default function loadBalancer(chinaDownload, USDownload) {
+
+}
