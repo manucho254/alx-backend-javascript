9465a8050813b33a1fe7c708a80101cdaa811bad
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-03 21:59:41 +0300

    fixes

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,8 +3,8 @@
 export default function handleResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      console.log('Got a response from the API');
       resolve({ status: 200, body: 'Success' });
+      console.log('Got a response from the API');
     } else {
       reject(
         (() => {
