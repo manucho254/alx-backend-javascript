3827ff2e746dd52ea1d9dc2317ca9280c4a60630
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-04 01:47:36 +0300

    Throw an error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -5,8 +5,9 @@ export default function guardrail(mathFunction) {
   const queue = [];
   try {
     queue.push(mathFunction());
+    queue.push('Guardrail was processed');
   } catch (error) {
-    queue.push(error);
+    queue.push(error.message);
     queue.push('Guardrail was processed');
   }
 
