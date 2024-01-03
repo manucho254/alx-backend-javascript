993462f0c1dbb57f8cad960919212819b687c858
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-04 00:48:07 +0300

    handle multiple promises

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -3,6 +3,8 @@
 
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise.any([chinaDownload, USDownload]).then((data) => {
-    console.log(data);
+    if (data) {
+      console.log(data);
+    }
   });
 }
