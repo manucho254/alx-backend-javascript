8cbee5132548d97e1d3486064ea5fccb5bd7e1f3
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-04 02:10:38 +0300

    async function named asyncUploadUser

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 /* eslint-disable no-unused-vars */
 
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 
 export default async function asyncUploadUser() {
   try {
-    const user = await signUpUser();
-    const photo = await uploadPhoto();
+    const user = await signUpUser().then((data) => data);
+    const photo = await uploadPhoto().then((data) => data);
 
     return { user, photo };
   } catch (error) {
