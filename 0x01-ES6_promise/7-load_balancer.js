486a9ddd90242ae0739744f281f03ffa54355835
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-04 00:42:42 +0300

    load balancer

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-unused-vars */
 
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.race([chinaDownload, USDownload]).then((data) => {
+  return Promise.any([chinaDownload, USDownload]).then((data) => {
     console.log(data);
   });
 }
