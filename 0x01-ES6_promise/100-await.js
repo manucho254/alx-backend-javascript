8440a7bcabf6460a9fe93bb10902051d2231867b
manucho254 <manuchoadero@gmail.com>
2024-01-03 23:14:46 +0000

    fixes

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -8,9 +8,10 @@ export default async function asyncUploadUser() {
   try {
     const user = await signUpUser().then((data) => data);
     const photo = await uploadPhoto().then((data) => data);
-
+	
     return { user, photo };
   } catch (error) {
+    console.log(error)
     return { user: null, photo: null };
   }
 }
