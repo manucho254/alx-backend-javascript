59d4b1d82dbc3a74546a30130f4f440a4e22bfe5
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-03 21:24:52 +0300

    return a promise

---
+++ 0x01-ES6_promise/0-main.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import getResponseFromAPI from './0-promise';
+
+const response = getResponseFromAPI();
+console.log(response instanceof Promise);
---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    resolve(true);
+  });
+}
