2bafeccdaac918c2b295a2ba54b26fb447532262
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-03 19:21:41 +0300

    using for of instead of for in

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+export default function appendToEachArrayValue(array, appendString) {
+  for (const idx of array) {
+    const value = array[idx];
+    array[idx] = appendString + value;
+  }
+
+  return array;
+}
---
+++ 0x00-ES6_basic/10-main.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import appendToEachArrayValue from "./10-loops";
+
+console.log(
+  appendToEachArrayValue(["appended", "fixed", "displayed"], "correctly-")
+);
