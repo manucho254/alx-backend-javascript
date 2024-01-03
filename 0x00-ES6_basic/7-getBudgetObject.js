9f6bea54e7659b69ab4dee7dc0e8b31e7cf2adee
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-03 20:49:32 +0300

    get budget objects

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = { income, gdp, capita };
-
-  return budget;
+  return { income, gdp, capita };
 }
