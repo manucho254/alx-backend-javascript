76e8b3bda6943925fe58232e09d3548e98da2fa1
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-04 00:39:21 +0300

    load balancer

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,6 +4,6 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup() {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(), uploadPhoto()]);
 }
