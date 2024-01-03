f72c76c3cc074eaff6d7c3d1a657b5ecbc374f71
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-03 21:50:42 +0300

    resolve and reject a promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,12 +3,12 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve({ status: 200, body: "Success" });
+      resolve({ status: 200, body: 'Success' });
     } else {
       reject(
         (() => {
-          throw new Error("The fake API is not working currently");
-        })()
+          throw new Error('The fake API is not working currently');
+        })(),
       );
     }
   });
