8aa25d3608ff93243a103819ad5de51b837f6bf1
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-04 22:44:06 +0300

    Implement a class named HolbertonCourse

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -3,6 +3,13 @@
 
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('TypeError: Name must be a string');
+    } else if (typeof length !== 'number') {
+      throw new TypeError('TypeError: Length must be a Number');
+    } else if (typeof students !== 'object') {
+      throw new TypeError('TypeError: Students must be an Array of Strings');
+    }
     this._name = name;
     this._length = length;
     this._students = students;
