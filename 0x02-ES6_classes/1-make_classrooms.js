4de14d6d2f264453c56264ce5e244c78fe9b77f8
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-05 02:15:18 +0300

    code fixes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 /* eslint-disable no-unused-vars */
 
-import ClassRoom from './0-classroom';
+class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
 
 export default function initializeRooms() {
   return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
