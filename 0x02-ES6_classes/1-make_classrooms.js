652a0ef0578cba56a0442c83d106da42eab31a8c
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-05 02:05:42 +0300

    code fixes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -4,9 +4,5 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const size19 = new ClassRoom(19);
-  const size20 = new ClassRoom(20);
-  const size34 = new ClassRoom(34);
-
-  return [size19, size20, size34];
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
