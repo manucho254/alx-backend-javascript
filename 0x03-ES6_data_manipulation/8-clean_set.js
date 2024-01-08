39ac7774aa60eff6fbe8c4f16056a2b4ff5d844c
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-09 00:29:24 +0300

    code fixes

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,20 +1,23 @@
 export default function cleanSet(set, startString) {
   let newStr = '';
-  if (startString.length === 0) {
+  if (startString.length === 0 || !(set instanceof Set)) {
     return newStr;
   }
+
   set.forEach((value) => {
-    if (value.includes(startString)) {
-      if (value.startsWith(startString)) {
-        if (newStr.length === 0) {
-          newStr += `${value.replace(startString, '')}`;
+    if (typeof value === 'string') {
+      if (value.includes(startString)) {
+        if (value.startsWith(startString)) {
+          if (newStr.length === 0) {
+            newStr += `${value.replace(startString, '')}`;
+          } else {
+            newStr += `-${value.replace(startString, '')}`;
+          }
+        } else if (newStr.length === 0) {
+          newStr += `${value}`;
         } else {
-          newStr += `-${value.replace(startString, '')}`;
+          newStr += `-${value}`;
         }
-      } else if (newStr.length === 0) {
-        newStr += `${value}`;
-      } else {
-        newStr += `-${value}`;
       }
     }
   });
