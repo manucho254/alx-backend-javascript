2d2a658384dce07f0c553fa57f7e5ba24e69f43f
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-08 15:27:57 +0300

    fixes to code

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,11 @@
-#!/usr/bin/env node
 /* eslint-disable no-unused-vars */
 
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(), uploadPhoto()]);
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
 }
