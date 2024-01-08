ab606e1675d2fd5ecab3b331555a92b3f9797ed1
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-08 21:07:17 +0300

    fixes to code

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const intBuff = new Int8Array(buffer);
-  const view = new DataView(buffer);
 
   try {
     intBuff[position] = value;
-    return view;
+    return buffer;
   } catch (error) {
     throw new Error('Position outside range');
   }
