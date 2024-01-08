d9b6f74700a8bbff821a0c4fda57e335ebb998f8
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-09 01:31:59 +0300

    weakmap to track number of requests

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,15 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  if (weakMap.get(endpoint) === undefined) {
+    weakMap.set(endpoint, 1);
+  } else {
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  }
+
+  if (weakMap.get(endpoint) >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
+
+export { weakMap, queryAPI };
