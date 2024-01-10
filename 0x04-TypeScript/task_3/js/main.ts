658781dcd5c678ecf2856c567ec118a8b85a2c0e
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-10 20:25:27 +0300

    String literal types

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,13 @@
+/** string literal types */
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  } else {
+    return "Teaching History";
+  }
+}
+
+console.log(teachClass("Math"));
+console.log(teachClass("History"));
