2033f2927e98d09acea69ce72202122abbdc8827
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-10 16:30:42 +0300

    Writing a class typscript

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -32,3 +32,51 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 console.log(director1);
+
+/** Interface printTeacher */
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+/** Write a function printTeacher */
+
+const printTeacher: printTeacherFunction = function (
+  firstName: string,
+  lastName: string
+): string {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe"));
+
+/** implement Student class and write an interface for it */
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
+}
+
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+const StudentClass: StudentConstructor = class StudentClass
+  implements StudentInterface
+{
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework() {
+    return "Currently working";
+  }
+  displayName() {
+    return this.firstName;
+  }
+};
+
+let student: StudentInterface = new StudentClass("Manucho", "dev");
+console.log(student.workOnHomework());
+console.log(student.displayName());
