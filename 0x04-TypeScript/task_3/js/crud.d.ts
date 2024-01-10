7044e72bda3845e412a3d0d57ae5fc4c155d8ffa
Emmanuel manucho <manuchoadero@gmail.com>
2024-01-10 23:28:36 +0300

    Ambient Namespaces

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,17 @@
+import { RowID, RowElement } from "./interface";
+
+/**
+ * type declarations for each crud function
+ */
+
+export function insertRow(row: RowElement): RowID {
+  /** function insert row  */
+}
+
+export function updateRow(newRowID: number, updatedRow: RowElement): number {
+  /** function update row  */
+}
+
+export function deleteRow(newRowID: number): void {
+  /** function delete row  */
+}
--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,4 +1,9 @@
 /** string literal types */
+/// <reference path="./crud.d.ts"/>
+
+import { RowElement, RowID } from "./interface";
+import * as CRUD from "./crud";
+
 type Subjects = "Math" | "History";
 
 function teachClass(todayClass: Subjects): string {
@@ -11,3 +16,18 @@ function teachClass(todayClass: Subjects): string {
 
 console.log(teachClass("Math"));
 console.log(teachClass("History"));
+
+let row: RowElement = {
+  firstName: "Guillaume",
+  lastName: "Salva",
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+const updatedRow: RowElement = {
+  firstName: "Guillaume",
+  lastName: "Salva",
+  age: 23,
+};
+
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
