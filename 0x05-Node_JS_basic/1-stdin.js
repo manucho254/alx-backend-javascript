14c31773b670f5f1965730f65c3da80bc4d9b9ea
Emmanuel manucho <manuchoadero@gmail.com>
2024-02-26 23:53:57 +0300

    Executing basic javascript with Node JS

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,10 +4,10 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
-    process.stdout.write(`Your name is: ${chunk}`);
+    console.log(`Your name is: ${chunk}`);
   }
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing');
+  console.log('This important software is now closing');
 });
