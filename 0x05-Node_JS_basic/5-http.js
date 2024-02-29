8acb04e3df421d0850b5f04c978711a1291107a5
Emmanuel manucho <manuchoadero@gmail.com>
2024-02-29 18:08:29 +0300

    fixes

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,29 +1,29 @@
-const http = require("http");
-const url = require("url");
-const countStudents = require("./3-read_file_async");
+const http = require('http');
+const url = require('url');
+const countStudents = require('./3-read_file_async');
 
 const fileName = process.argv[2];
 
 // Make our HTTP server
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
+  res.setHeader('Content-Type', 'text/plain');
   const reqUrl = url.parse(req.url).pathname;
 
-  if (reqUrl === "/") {
-    res.end("Hello Holberton School!");
-  } else if (reqUrl === "/students") {
-    let resData = "";
-    resData += "This is the list of our students\n";
+  if (reqUrl === '/') {
+    res.end('Hello Holberton School!');
+  } else if (reqUrl === '/students') {
+    let resData = '';
+    resData += 'This is the list of our students\n';
 
     countStudents(fileName)
       .then((data) => {
         resData += `Number of students: ${data.size}`;
         for (const [key, val] of Object.entries(data.fields)) {
-          const splitKey = key.split("\r")[0];
+          const splitKey = key.split('\r')[0];
           resData += `\nNumber of students in ${splitKey}: ${
             val.length
-          }. List: ${val.join(", ")}`;
+          }. List: ${val.join(', ')}`;
         }
         res.end(resData);
       })
