cf91ae02df0c49040f01daa079df726dde5c91a7
Emmanuel manucho <manuchoadero@gmail.com>
2024-02-29 18:07:36 +0300

    fixes

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,37 +1,36 @@
-const http = require('http');
-const url = require('url');
-const countStudents = require('./3-read_file_async');
+const http = require("http");
+const url = require("url");
+const countStudents = require("./3-read_file_async");
 
 const fileName = process.argv[2];
 
 // Make our HTTP server
 const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
   const reqUrl = url.parse(req.url).pathname;
-  if (reqUrl === '/') {
-    res.write('Hello Holberton School!');
-    res.end();
-  } else if (reqUrl === '/students') {
-    res.write('This is the list of our students');
+
+  if (reqUrl === "/") {
+    res.end("Hello Holberton School!");
+  } else if (reqUrl === "/students") {
+    let resData = "";
+    resData += "This is the list of our students\n";
+
     countStudents(fileName)
       .then((data) => {
-        res.write(`Number of students: ${data.size}`);
-
+        resData += `Number of students: ${data.size}`;
         for (const [key, val] of Object.entries(data.fields)) {
-          const splitKey = key.split('\r')[0];
-          res.write(
-            `Number of students in ${splitKey}: ${val.length}. List: ${val.join(
-              ', ',
-            )}`,
-          );
+          const splitKey = key.split("\r")[0];
+          resData += `\nNumber of students in ${splitKey}: ${
+            val.length
+          }. List: ${val.join(", ")}`;
         }
-        res.end();
+        res.end(resData);
       })
       .catch((error) => {
-        res.write(`${error}`);
+        resData += `${error}`;
+        res.end(resData);
       });
-
-    res.end();
   }
 });
 // Have the server listen on port 1245
