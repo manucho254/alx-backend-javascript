9df1deac060a15d5fd3ed70c5c583bd897f4f64b
Emmanuel manucho <manuchoadero@gmail.com>
2024-03-01 02:14:26 +0300

    Deep equality & Post integration testing

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,46 +1,51 @@
 const request = require("request");
 const { expect } = require("chai");
-const URL = "http://127.0.0.1:7865";
 
 describe.only("Api Integration test", function () {
   it("Right data in response /", function () {
-    request(`${URL}/`, function (error, response, body) {
+    request("http://127.0.0.1:7865/", function (error, response, body) {
       expect(response.statusCode).to.be.equal(200);
       expect(body).to.be.equal("Welcome to the payment system");
     });
   });
   it("404 if path does not exist /test", function () {
-    request(`${URL}/test`, function (error, response, body) {
+    request("http://127.0.0.1:7865/test", function (error, response, body) {
       expect(response.statusCode).to.be.equal(404);
     });
   });
   it("Right data in response /cart", function () {
-    request(`${URL}/cart/123`, function (error, response, body) {
+    request("http://127.0.0.1:7865/cart/123", function (error, response, body) {
       expect(response.statusCode).to.be.equal(200);
       expect(body).to.be.equal("Payment methods for cart 123");
     });
   });
   it("404 if path does not exist /cart", function () {
-    request(`${URL}/cart/test`, function (error, response, body) {
-      expect(response.statusCode).to.be.equal(404);
-    });
+    request(
+      "http://127.0.0.1:7865/cart/test",
+      function (error, response, body) {
+        expect(response.statusCode).to.be.equal(404);
+      }
+    );
   });
 
   it("Right data in response /available_payments", function () {
-    request(`${URL}/available_payments`, function (error, response, body) {
-      const data = JSON.stringify({
-        payment_methods: {
-          credit_cards: true,
-          paypal: false,
-        },
-      });
-      expect(response.statusCode).to.be.equal(200);
-      expect(body).to.be.equal(data);
-    });
+    request(
+      "http://127.0.0.1:7865/available_payments",
+      function (error, response, body) {
+        const data = JSON.stringify({
+          payment_methods: {
+            credit_cards: true,
+            paypal: false,
+          },
+        });
+        expect(response.statusCode).to.be.equal(200);
+        expect(body).to.be.equal(data);
+      }
+    );
   });
   it("Right response /login", function () {
     const options = {
-      uri: `${URL}/login`,
+      uri: "http://127.0.0.1:7865/login",
       method: "POST",
       json: true,
       body: { userName: "Manucho" },
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,28 +1,30 @@
 const request = require("request");
 const { expect } = require("chai");
-const URL = "http://127.0.0.1:7865";
 
 describe.only("Api Integration test", function () {
   it("Right data in response /", function () {
-    request(`${URL}/`, function (error, response, body) {
+    request("http://127.0.0.1:7865/", function (error, response, body) {
       expect(response.statusCode).to.be.equal(200);
       expect(body).to.be.equal("Welcome to the payment system");
     });
   });
   it("404 if path does not exist /", function () {
-    request(`${URL}/test`, function (error, response, body) {
+    request("http://127.0.0.1:7865//test", function (error, response, body) {
       expect(response.statusCode).to.be.equal(404);
     });
   });
   it("Right data in response /cart", function () {
-    request(`${URL}/cart/123`, function (error, response, body) {
+    request("http://127.0.0.1:7865/cart/123", function (error, response, body) {
       expect(response.statusCode).to.be.equal(200);
       expect(body).to.be.equal("Payment methods for cart 123");
     });
   });
   it("404 if path does not exist /cart", function () {
-    request(`${URL}/cart/test`, function (error, response, body) {
-      expect(response.statusCode).to.be.equal(404);
-    });
+    request(
+      "http://127.0.0.1:7865/cart/test",
+      function (error, response, body) {
+        expect(response.statusCode).to.be.equal(404);
+      }
+    );
   });
 });
